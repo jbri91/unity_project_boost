5eeec146a05088707497b4ff7959ec8e7718f435
Johnny Briones <jbrrii91@gmail.com>
2022-11-23 03:35:53 -0500

    Create second scene. Rename scenes to over and under. Create condition for reload to next level and/or first level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
   
-    void Start()
+
+   void OnCollisionEnter(Collision other) 
+   {
+     
+     switch (other.gameObject.tag)
+     {
+        case "Friendly":
+            Debug.Log("Friendly object was hit...");
+            break;
+        case "Fuel":
+            Debug.Log("Fuel object was hit...");
+            break;
+        case "Finish":
+            Debug.Log("Congrats you finished!");
+            LoadNextLevel();
+            break;
+        default:
+            Debug.Log("Sorry you blew up!");
+            ReloadLevel();
+            break;
+     }
+   }
+
+    void LoadNextLevel()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0;
+            }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
-  
-    void Update()
+    void ReloadLevel()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
+
 }
